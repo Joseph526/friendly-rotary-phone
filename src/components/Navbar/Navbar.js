8f9886214c7b01c6cc45f06e7801aad79578d2c5
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-22 23:52:44 -0400

    Add navbar elements

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+const Navbar = props => (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <a className="navbar-brand" href="index.html">ClickMatchWin</a>
+    </nav>
+);
+
+export default Navbar;
