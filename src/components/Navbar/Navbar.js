f5fcbedfa29af12eedaee7cca283852c209d64c2
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-23 20:50:43 -0400

    Add navbar details and custom CSS

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,15 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = props => (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-md navbar-dark bg-danger justify-content-between">
         <a className="navbar-brand" href="index.html">ClickMatchWin</a>
+        <div className="navbar-text">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">Score: |&nbsp;</li>
+                <li className="nav-item">Top Score: </li>
+            </ul>
+        </div>
     </nav>
 );
 
