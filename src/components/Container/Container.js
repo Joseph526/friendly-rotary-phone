ff3e46c4ac6d2f5064821242bc908b6169ef543d
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-24 23:06:38 -0400

    Use row in container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Row from "../Row";
 
 const Container = props => (
     <main className="container-fluid">
-    
+        <div className="album py-5">
+            <div className="container">
+                <Row />
+                <Row />
+                <Row />
+            </div>
+        </div>
     </main>
 );
 
