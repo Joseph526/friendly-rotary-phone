456f4cfdea2555d82d43f3ea9ed8d957bf9a6235
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-24 20:50:13 -0400

    Add game instructions to jumbotron

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -3,6 +3,7 @@ import React from "react";
 const Jumbotron = props => (
     <div className="jumbotron bg-light text-center">
         <h1>Baseball Card Match</h1>
+        <h3>Click on a baseball card to earn points, but don't click on the same one twice!</h3>
     </div>
 );
 
