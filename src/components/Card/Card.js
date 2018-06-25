8b0bbb14643d22af301c6d668e0f7be691a37270
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-24 23:24:29 -0400

    Use placeholder image in card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,14 +2,7 @@ import React from "react";
 
 const Card = props => (
     <div className="card mb-4 box-shadow">
-        <img className="card-img-top" src="#" alt="Placeholder" />
-        <div className="card-body">
-            <h5 className="card-title">NewsCatcher</h5>
-            <p className="card-text">News aggregator app using Node, Express and Mongoose</p>
-            <div className="d-flex justify-content-between align-items-center">
-                <small className="text-muted">JavaScript</small>
-            </div>
-        </div>
+        <img className="card-img-top" src="http://via.placeholder.com/300" alt="Placeholder" />
     </div>
 );
 
