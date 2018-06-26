0d625239e382810dd97387a1351576dc92729f9c
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-25 21:03:28 -0400

    Use sticky-top

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = props => (
-    <nav className="navbar navbar-expand-md navbar-dark bg-danger justify-content-between">
+    <nav className="navbar sticky-top navbar-expand-md navbar-dark bg-danger justify-content-between">
         <a className="navbar-brand" href="index.html">ClickMatchWin</a>
         <div className="navbar-text">
             <ul className="navbar-nav mr-auto">
